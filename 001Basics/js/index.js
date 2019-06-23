fced694c154a135952f9e73f7153fd67bb87bb84
zed-Q <52119879+zed-Q@users.noreply.github.com>
2019-06-23 16:24:44 +0800

    Update index.js

--- 001Basics/js/index.js
+++ 001Basics/js/index.js
@@ -1,1 +1,54 @@
+/**
+ *
+ * Created by Administrator on 2018/1/30 0030.
+ */
+var i=0;
+var timer;
+$(window).ready(function () {
 
+   $('.ig').eq(0).show().siblings('.ig').hide();
+
+   showTimer();
+
+  $('.left_buttom').click(function () {
+      clearInterval(timer);
+      i--;
+      if(i==-1){
+          i=3;
+      }
+      Show();
+      showTimer()
+
+  })
+
+    $('.right_buttom').click(function () {
+        clearInterval(timer);
+        i++;
+        if(i==4){
+            i=0;
+        }
+
+        Show();
+        showTimer()
+    })
+});
+
+
+
+//会动的定时器
+function showTimer() {
+     clearInterval(timer)
+    timer=setInterval(function () {
+        Show();
+        i++;
+        if(i==4){
+            i=0;
+        }
+    },2500)
+
+}
+//会动的方法
+
+function Show() {
+    $('.ig').eq(i).fadeIn(300).siblings('.ig').fadeOut(300);
+}
